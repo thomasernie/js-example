b215dd244e1dc3cfbdf3eee0d6356897ab159eab
balaji <balaji.a@fr8.in>
2020-05-21 11:36:36 +0530

    checking autosave

--- example2.js
+++ example2.js
@@ -5,7 +5,6 @@ var app = express();
 app.get('/hello',function(req,res){
     res.send("hello fr8!");
 });
-
 app.post('/hello',function(req,res)
 {
 res.send("you just called the post method at`/hello`!\n");
