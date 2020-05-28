22f43c5dc9f727649d6757b25587a76249e6d327
Suresh <suresh.as@fr8.in>
2020-05-28 13:35:16 +0530

    added ch2

---
+++ programminLevels.js
@@ -0,0 +1,22 @@
+//Begineers - 3 - 6 months
+
+const basicsStrong = true
+const errorFixing = function copyErrorCheckINGoogleRead() {
+    const googleResult = []
+    const resultIntIme = googleResult.find(result === 'solution')
+    if (resultIntIme > 4) // more than 4 hrs
+    {
+        return "getHelp"
+    }
+}
+
+// Intermeditate - 6 months to 2 Years
+const specializeOneTech = true
+const deepKnowledgeinOneTech = true
+const knowsMostOfCommonModules = true
+const independentMostOfTheTime = true
+
+// Architect - 2 Years
+const startSettingFramework = true
+const startSettingStandards = true
+const startReviewinGCode = trues
---
+++ string.js
@@ -0,0 +1,25 @@
+// single quote
+
+const varString = "Jay"
+let nameString = "Hello " + "Jay " + 1 + true
+
+console.log("single:", nameString)
+// single quote
+nameString = 'Hello ' + varString + 1 + true + " Working"
+
+console.log("double:", nameString)
+
+// + overload quote
+nameString = 1 + 1 + 1 + false + " Working \n hi"
+
+console.log("double:", nameString)
+
+// forward slsh
+nameString = `Hi ${vrString}
+hi`
+
+console.log("forward` :", nameString)
+// alert is for front end f=and console log for
+
+// 
+//
