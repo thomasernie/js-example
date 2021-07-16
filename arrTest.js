5485dc5ba2b6227b1199108a0f3c1e0e3532e29d
NithishwarS <nithishwar.s@fr8.in>
2021-07-16 06:04:29 +0530

    arr
    example

--- arrTest.js
+++ arrTest.js
@@ -5,11 +5,11 @@ const Input = {
         { id: 3, branchId: 22, studentId: 1 },
         { id: 4, branchId: 10, studentId: 3 }
     ],
-    branch: [
+    branches: [
         { id: 10, name: "CSE" },
         { id: 22, name: "IT" }
     ],
-    student: [
+    students: [
         { id: 1, sName: "Jay" },
         { id: 2, sName: "Sanjay" },
         { id: 3, sName: "Rajesh" }
@@ -17,36 +17,23 @@ const Input = {
 };
 function studentDetails(data) {
     const branchStudent = data.branchStudent;
-    const branch = data.branch;
-    const student = data.student;
-
-    let details = {};
-
-    const branchStudentMap = branchStudent.map(obj => {
-
-        const branch_data = branch.filter(val => {
-            const branchName = obj.branchId === val.id;
-            return branchName;
-        });
-        const [branchData] = branch_data;
-
-        const students = student.filter(val => {
-            const students = obj.studentId === val.id;
-            return students;
-        });
-        const [studentName] = students;
-
-        details = {
-            id: obj.id,
-            branchId: obj.branchId,
-            branchName: branchData.name,
-            studentId: obj.studentId,
-            studentName: studentName.sName
+    const branches = data.branches;
+    const students = data.students;
+    const branchStudentMap = branchStudent.map(branchDatas => {
+        return {
+            ...branchDatas,
+            branchName: branches.find(branch => {
+                const branchName = branchDatas.branchId === branch.id;
+                return branchName
+            })?.name,
+            studentName: students.find(student => {
+                const studentName = branchDatas.studentId === student.id;
+                return studentName
+            })?.sName,
         }
-        return details;
     })
     console.log(branchStudentMap);
 }
 studentDetails(Input);
 
-//TO-DO do the above with map and filter 
+//TO-DO do the above with map and filter
