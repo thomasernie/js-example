fdf9f1bfbbc6acc435211d8b2db9cb73230fd9fc
Dhanalaxmi-Shettiyar <dhanalaxmi.s@fr8.in>
2021-07-19 11:17:15 +0530

    Update imposition.js

--- imposition.js
+++ imposition.js
@@ -1,5 +1,5 @@
 const i ="We are frugal. We value our money, time & resources and ensure they are deployed in the most productive way";
-for (j = 0; j <= 3 ; j++)
+for (j = 0; j <= 100 ; j++)
 {
   console.log("owners= ",i);
-  
+}
