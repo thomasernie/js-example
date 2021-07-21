27a97bea20cf49af2cccf513347571837589bd3d
vijayat2 <87650249+vijayat2@users.noreply.github.com>
2021-07-21 18:30:20 +0530

    update imposition.js

--- imposition.js
+++ imposition.js
@@ -1,5 +1,5 @@
-const i ="We are frugal. We value our money, time & resources and ensure they are deployed in the most productive way";
-for (j = 0; j <= 3 ; j++)
+const i ="We act like owners.We are frugal. We value our money, time & resources and ensure they are deployed in the most productive way";
+for (j = 0; j <= 100 ; j++)
 {
-  console.log("owners = ",i);
+  console.log("our code = ",i);
 }
