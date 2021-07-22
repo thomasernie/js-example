de85d278b795aaeebb04300d0860f36abf3716b5
j <jay@fr8.in>
2021-07-22 15:33:49 +0530

    changes

--- print100.js
+++ print100.js
@@ -1,15 +1,15 @@
-const i ="We act like owners.We are frugal. We value our money, time & resources and ensure they are deployed in the most productive way";
+const code ="We act like owners.We are frugal. We value our money, time & resources and ensure they are deployed in the most productive way";
 // for (j = 0; j < 5; j++)
 // {
 //   console.log("our code = ",i)
 // }
 
 
-function owner(print){
-  for (let j = 0; j < 5; j++)
+function print(message){
+  for (let j = 1; j <= 100; j++)
   
-    console.log(print)
+    console.log(j,message)
   
 }
 
-owner(i)
+print(code)
