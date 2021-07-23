cd23090a817f8e250e4e35d49ed725bf3da99d00
saarukaa <saarukaa.devi@fr8.in>
2021-07-23 16:16:38 +0530

    jsexercise

--- output2.js
+++ output2.js
@@ -35,15 +35,34 @@
     
 // })
 
-const    Input = 
-         [
-            { id: 1, branchId: 1, studentId: 1 },
-            { id: 2, branchId: 2, studentId: 2 },
-            { id: 3, branchId: 2, studentId: 1 },
-            { id: 4, branchId: 1, studentId: 3 }
-        ]
 
 
-        // filter
+const  Input = {
+            branchStudent: [
+                { id: 1, branchId: 1, studentId: 1 },
+                { id: 2, branchId: 2, studentId: 2 },
+                { id: 3, branchId: 2, studentId: 1 },
+                { id: 4, branchId: 1, studentId: 3 }
+            ],
+            branch: [
+                { id: 1, name: "CSE" },
+                { id: 2, name: "IT" }
+            ],
+            student: [
+                { id: 1, name: "Jay" },
+                { id: 2, name: "Sanjay" },
+                { id: 3, name: "Rajesh" }
+            ]
+        }
 
-        
+let start=Input.branchStudent
+let names=Input.branch
+let studen=Input.student
+
+
+
+
+let final=start.map((val)=>{
+    return(val.branchId)
+})
+  console.log(final)
