d2b21fe22ef0a1822deb0c77fd1e761715b07642
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-07-24 17:05:41 +0530

    map

--- map.js
+++ map.js
@@ -1,17 +1,27 @@
-const branchStudent: [
-    { id: 1, branchId: 1, studentId: 1 },
-    { id: 2, branchId: 2, studentId: 2 },
-    { id: 3, branchId: 2, studentId: 1 },
-    { id: 4, branchId: 1, studentId: 3 }
-];
-   
-const branch: [
-    { id: 1, name: "CSE" },
-    { id: 2, name: "IT" }
-],
-const student: [
-    { id: 1, name: "Jay" },
-    { id: 2, name: "Sanjay" },
-    { id: 3, name: "Rajesh" }
-]
-  console.log
+const Input = {
+    branchStudent: [
+        { id: 1, branchId: 1, studentId: 1 },
+        { id: 2, branchId: 2, studentId: 2 },
+        { id: 3, branchId: 2, studentId: 1 },
+        { id: 4, branchId: 1, studentId: 3 }
+    ],
+    branch: [
+        { id: 1, name: "CSE" },
+        { id: 2, name: "IT" }
+    ],
+    student: [
+        { id: 1, name: "Jay" },
+        { id: 2, name: "Sanjay" },
+        { id: 3, name: "Rajesh" }
+    ]
+}
+console.log(Input.branchStudent);
+
+ const branchStudent = Input.branchStudent;
+ const branch = Input.branch;
+ const student = Input.student;
+
+ 
+
+
+
