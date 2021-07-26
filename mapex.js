74ea37efc7ec208351a0730dc238e61cabd4b243
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-07-26 12:47:49 +0530

    map3

---
+++ mapex.js
@@ -0,0 +1,63 @@
+const Input = {
+    branchStudent: [
+        { id: 1, branchId: 1, studentId: 1 },
+        { id: 2, branchId: 2, studentId: 2 },
+        { id: 3, branchId: 2, studentId: 1 },
+        { id: 4, branchId: 1, studentId: 3 }
+    ],
+    branch: [
+        { id: 1, name: "CSE" },
+        { id: 2, name: "IT" }
+    ],
+    student: [
+        { id: 1, name: "Jay" },
+        { id: 2, name: "Sanjay" },
+        { id: 3, name: "Rajesh" }
+    ]
+};
+
+console.log(Input.branchStudent);
+
+   const branchStudent = Input.branchStudent;
+   const branch = Input.branch;
+   const student = Input.student;
+
+const branchDatas = Input.branch.find(myfunction);//
+
+function myfunction(value){
+     
+     return value;
+  }
+
+const studentDatas = Input.student.find(myfunction);
+
+function myfunction(value){
+    
+    return value;
+
+}
+//
+ 
+const datas = Input.branchStudent.map(function(value){
+     
+    return {
+         id:value.id,
+         branchId:value.branchId,
+         branchName:branchDatas.name,
+         studentId:value.studentId,
+         studentName:studentDatas.name,
+     };
+ })
+ console.log('branchStudentMap:',datas);
+ 
+
+ 
+
+
+
+
+
+ 
+
+
+
