076455e0f73b8ec75f2278b825c92edbd6521de8
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-07-27 23:26:11 +0530

    expres

--- Exercises/.js
+++ Exercises/.js
@@ -1,7 +1,6 @@
 const express = require('express');
 
 const app = express();
-const port = 3000;
 
 app.get('/', function(req, res){
    res.send("Hello world!");
---
+++ Exercises/Express1.js
@@ -0,0 +1,11 @@
+const express = require('express');//import express in our file..access through variable express 
+//create an application 
+const app = express();//assign it to const app
+
+const port =8080 ;
+
+//app.get(route,callback)
+//The callback function has 2 parameters, request(req) and response(res). 
+app.get('/',(req,res)=>res.send('Hello FR8'));
+
+app.listen(port,()=> console.log(`listening at http://localhost:${port}`));
