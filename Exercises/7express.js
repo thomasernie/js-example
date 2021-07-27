263ce765a6b11df4157a70d9f0d4dd6dc3881cbb
Dhanalaxmi-Shettiyar <dhanalaxmi.s@fr8.in>
2021-07-27 19:21:21 +0530

    new

--- Exercises/7express.js
+++ Exercises/7express.js
@@ -1,16 +1,22 @@
 const express = require('express')
 const app = express()
-//const bodyParser = require('body-parser');
-const port = 1200;
-app.get('/', (req,res) => {
-    res.send('Hello Jay')
-    //console.log('hello jay');
-})
-app.post('/',(req,res) => {
-    //res.send(name:"jay")
-    console.log('name:"jay"')
-}
-)
+const port = 3000;
+const fs = require('fs')
+
+// create get end point
+app.get('/hello', function(req,res) {
+    var queryParameter = req.query;
+    //console.log(queryParameter.name);
+    res.json(`Hello${queryParameter.name}`);
+});
+
+// //create a post endpoint
+// app.post('/hello',function (req,res){
+//     const data = req.body.name;
+//     console.log(data)
+//     res.send(data)
+// });
+
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`);
 })
