dabd83a393e96645ae64d9021262009ad5bab2c8
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-07-27 15:04:55 +0530

    ex

--- Exercises/Expres.js
+++ Exercises/Expres.js
@@ -1,9 +1,11 @@
-const express = require('express')
+const express = require('express')//import express in our file..access through variable express 
+//create an application 
+const app = express()//assign it to const app
 
-const app = express()
+const port = 3000
 
-const port = 3004
-
+//app.get(route,callback)
+//The callback function has 2 parameters, request(req) and response(res). 
 app.get('/',(req,res)=>res.send('FR8!'))
 
 app.get('/hello',(req,res)=>res.send('Hello FR8!'))
