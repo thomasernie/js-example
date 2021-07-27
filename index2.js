f2444ff306923855a433aaed9f0dbb71477a744e
saarukaa <saarukaa.devi@fr8.in>
2021-07-27 15:06:56 +0530

    expressexample

--- index2.js
+++ index2.js
@@ -1,9 +1,19 @@
+// const express = require('express')
+
+// const app = express()
+
+// const port = 3000
+
+// app.get('/read-users', (req, res) => res.send("list of users"))
+
+// app.listen(port, () => console.log("server is running"))
+
 const express=require('express')
 
 const app=express()
 
 const port=3000
 
-app.get('/',(req,res)=> res.send("hello fr8"))
+app.get('/hello',(req,res)=> res.send("jay"))
 
-app.listen(port,()=>console.log("server is running"))
+app.listen(3000)
