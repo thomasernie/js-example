fe4b451fe3677dd91f8a0278809423e8e1a500af
Dhanalaxmi-Shettiyar <dhanalaxmi.s@fr8.in>
2021-07-29 13:37:36 +0530

    Today

--- Exercises/7express.js
+++ Exercises/7express.js
@@ -4,49 +4,51 @@ bodyParser = require('body-parser');
 app.use(bodyParser.json());
 const port = 3000
 
-// create get end point
-// app.get('/hello', function(req,res) {
-//     const queryParameter = req.query;
-//     console.log(queryParameter.name);
-//     res.send(`hello${queryParameter.name}`);
-// });
+// To get Hello Jay
+app.get('/hello', function(req,res) {
+    const queryParameter = req.query;
+    console.log(queryParameter.name);
+    res.send(`hello${queryParameter.name}`);
+});
 
-//create a post endpoint
-// app.post('/hello',function (req,res){
-//    const postParameter = req.body
-//    console.log(postParameter)
-//    res.send(`hello${postParameter.name}`)
-//  });
+// To post Hello Jay
+app.post('/hello',function (req,res){
+   const postParameter = req.body
+   console.log(postParameter)
+   res.send(`{Output:hello${postParameter.name}}`)
+ });
+
+// To post Student
+function studentInfo(studentDatas){
+const branchStudents = studentDatas.branchStudents;
+    const branches = studentDatas.branches;
+    const students = studentDatas.students;
+    
+    const branchNameFinder = (branchId) => branches.find(branch => branch.id === branchId).name;
+    
+    const studentNameFinder = (studentId) => students.find(student => student.id === studentId).name;
+    
+    const branchStudentMapper = (branchStudents)  => 
+    { 
+        return {
+            id:branchStudents.id,
+            branchId:branchStudents.branchId,
+            branchName: branchNameFinder(branchStudents.branchId),
+            studentId: branchStudents.studentId,
+            studentName: studentNameFinder(branchStudents.studentId)
+        }
+    }
+    const branchStudentMaps = branchStudents.map(branchStudentMapper);
+    return branchStudentMaps;
+    
+};
 
 app.post('/student',function (req,res){
-    const branchStudentMaps = req.body;
-    res.send(branchStudentMaps)
+    const input = req.body
+    res.send(studentInfo(input))
+    //console.log('{branchStudentMap${res.send}}')
 });
-     
-// const branchStudents = studentDatas.branchStudents;
-//     const branches = studentDatas.branches;
-//     const students = studentDatas.students;
-    
-//     const branchNameFinder = (branchId) => branches.find(branch => branch.id === branchId).name;
-    
-//     const studentNameFinder = (studentId) => students.find(student => student.id === studentId).name;
-    
-//     const branchStudentMapper = (branchStudent)  =>
-//     { 
-//         return {
-//             id:branchStudent.id,
-//             branchId:branchStudent.branchId,
-//             branchName: branchNameFinder(branchStudent.branchId),
-//             studentId: branchStudent.studentId,
-//             studentName: studentNameFinder(branchStudent.studentId)
-//         }
-//     };
-//     const branchStudentMaps = branchStudents.map(branchStudentMapper);
-    
-//     console.log('branchStudentMap =', branchStudentMaps);
-//     //res.end(JSON.stringify(branchStudentMaps)); 
-   
-
 app.listen(port, () => {
-    console.log(`app listening at http://localhost:${port}`);
+    console.log(`app listening at http://localhost:${port}`)
 })
+
