e76a34d6d9823d9dd254a527857fc46309c1697d
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-07-29 10:56:44 +0530

    express

--- Exercises/express3.js
+++ Exercises/express3.js
@@ -2,19 +2,14 @@ const express = require('express');
 
 const app = express();
 
-bodyParser = require('body-parser')
+bodyParser = require('body-parser');
 
-app.use(bodyParser.json());// Parses the text as JSON and exposes the resulting object on req.body.
+app.use(bodyParser.json());//JSON.parse() method takes a string as input and transforms it into an object.
+// Parses the text as JSON and exposes the resulting object on req.body.
 
 const port = 7665;
 
-     //app.get('/Hello',function(req,res){
-
-    //const queryParam = req.query;
-
-    //console.log(queryParam.name);
-
-    //res.send(`Hello${queryParam.name}`);
+     
     app.post('/Hello',(req,res)=>{
 
        const param = req.body;
