9ac6214c91379a52db5d4a40f2ccdcef1f7427f5
NithishwarS <nithishwar.s@fr8.in>
2021-08-01 10:41:08 +0530

    react
    react

---
+++ ReactApp/counter/counter-app/src/components/counter.jsx
@@ -0,0 +1,34 @@
+import {useState} from 'react';
+import styles from '../appStyles.module.css'
+
+
+function Counter(){
+        return (<div><Count /></div>)
+}
+
+function Count(props){
+    let [count,setCount] = useState(0);
+    function minus(){
+        setCount(count - 1) ;
+    }
+    function plus(){
+        setCount(count + 1) ;
+    }
+    function reset(){
+        setCount(count = 0)
+    }
+    return (
+        <div>
+            <div className = {styles.header}>
+                <div>
+            <span className = {styles.count}>{count}</span>
+            <button className = {styles.incBtn} onClick = {minus}>-</button>
+            <button className = {styles.decBtn} onClick = {plus}>+</button>
+            <button className = {styles.reset}  onClick = {reset}>RESET</button>
+            </div>
+            </div>
+            </div>
+    )
+}
+
+export default Counter;
