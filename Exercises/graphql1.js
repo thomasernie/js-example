241da6a9a54b36f4aee342ebe6154600f1a813fe
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-08-04 16:10:47 +0530

    ql

--- Exercises/graphql1.js
+++ Exercises/graphql1.js
@@ -1,20 +1,19 @@
- const{ graphql, buildSchema } = require('graphql');
-
-// Construct a schema, using GraphQL schema language
- schema = buildSchema(`
-  type Query {
-    hello: String
-  }
-`);
-
-// The root provides a resolver function for each API endpoint
-const root = {
-  hello: () => {
-    return 'Hello world!';
-  },
-};
-
-// Run the GraphQL query '{ hello }' and print out the response
-graphql(schema, '{ hello }', root).then((response) => {
-  console.log(response);
-});
+//Execute GraphQL query with a command-line tool
+var { graphql, buildSchema } = require('graphql');  
+// A schema is constructed using GraphQL schema language  
+var schema = buildSchema(`  
+  type Query {  
+    hello: String  
+  }  
+`);  
+// The root provides a resolver function for each API endpoint  
+var root = {  
+  hello: () => {  
+    return 'Hello world! This is the first GrapghQL query';  
+  },  
+};  
+// Run the GraphQL query '{ hello }' and print out the response  
+graphql(schema, '{ hello }', root).then((response) => {  
+    console.log(response);  
+  });  
+  //This is a simple GraphQL query that you executed on command prompt without using a server.
