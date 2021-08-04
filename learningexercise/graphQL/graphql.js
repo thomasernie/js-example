fb9a0970544db4ba3061016e6ec7de7d5c02edf0
vijayat2 <87650249+vijayat2@users.noreply.github.com>
2021-08-04 19:28:05 +0530

    bb

--- learningexercise/graphQL/graphql.js
+++ learningexercise/graphQL/graphql.js
@@ -11,3 +11,21 @@
 // app.listen(4000,()=>{
 //     console.log('server running on port 4000')
 // });
+
+
+
+const express = require ('express');
+const app = express()
+const {GraphQLClient,Gql} = require ('graphql-request');
+ async function main(){
+     const endpoint = 'https://dcore.fr8.in/v1/graphql'
+ }
+ const query = gql`
+ query city($search: String, $limit: Int) {
+    city(where: {_and: [{name: {_ilike: $search}}, {is_connected_city: {_eq: true}}]}, limit: $limit) {
+      id
+      name
+      is_connected_city
+    }
+  }
+`
