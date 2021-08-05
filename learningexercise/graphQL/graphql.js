68ae587fbfd2de3850d3132d01cb5ce6b65e33d4
vijayat2 <87650249+vijayat2@users.noreply.github.com>
2021-08-05 13:36:25 +0530

    aaa

--- learningexercise/graphQL/graphql.js
+++ learningexercise/graphQL/graphql.js
@@ -1,27 +1,14 @@
-// const express = require ('express');
-// const graphql = require ('graphql')
-// const app = express();
-// const {graphqlHTTP} = require('express-graphql')
-// app.use('/graphql',graphqlHTTP({
-//     schema: schema,
-//   rootValue: root,
-//   graphiql: true,
-
-// }));
-// app.listen(4000,()=>{
-//     console.log('server running on port 4000')
-// });
-
-
-
 const express = require ('express');
 const app = express()
-const {GraphQLClient,Gql} = require ('graphql-request');
- async function main(){
-     const endpoint = 'https://dcore.fr8.in/v1/graphql'
- }
- const query = gql`
- query city($search: String, $limit: Int) {
+const { GraphQLClient, gql} =require('graphql-request')//this package is used to send query request to graphql server
+ 
+  const graphQLClient = new GraphQLClient('https://dcore.fr8.in/v1/graphql') //giving a new operatorthe built-in object types that has a constructor function.
+  console.log(graphQLClient)                                                 //giving  the graphqlclient a url that we need response from that graphql server
+
+app.get('/city', async (request, response)=> { 
+                                                                              
+  const query = gql `                                  
+  query city($search: String, $limit: Int) {
     city(where: {_and: [{name: {_ilike: $search}}, {is_connected_city: {_eq: true}}]}, limit: $limit) {
       id
       name
@@ -29,3 +16,20 @@ const {GraphQLClient,Gql} = require ('graphql-request');
     }
   }
 `
+const searchingCity = `%${request.query.name}%`;
+const variables = {
+  search:searchingCity, // this search based on the name we give in the query parameter  //used % % which it searches any words containing this form letters    
+  limit:5
+}
+   const city = await graphQLClient.request(query,variables)
+    response.send(city)
+
+}) 
+
+ 
+
+ 
+  
+
+ 
+app.listen(5000,() => console.log("the server started at http://localhost:5000"));
