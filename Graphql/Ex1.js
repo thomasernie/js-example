5c2a595e27d1ec5f56eea6bcbbeeabbb1b6accda
Dhanalaxmi-Shettiyar <dhanalaxmi.s@fr8.in>
2021-08-06 10:20:41 +0530

    Update Ex1.js

--- Graphql/Ex1.js
+++ Graphql/Ex1.js
@@ -5,11 +5,11 @@ const {GraphQLClient, gql} = require ('graphql-request')
 //app.use(bodyParser.json());
 const port= 1000;
 
-app.get('/city', async(req,res) =>{
+app.get('/city', async(req,res) => {
   const endpoint = 'https://dcore.fr8.in/v1/graphql';
   const graphqlClient = new GraphQLClient(endpoint)
 
-  const query = gql`
+const query = gql`
     query city($search: String, $limit: Int) {
     city(where: {_and: [{name: {_ilike: $search}}, {is_connected_city: {_eq: true}}]}, limit: $limit) {
     id
@@ -18,23 +18,14 @@ app.get('/city', async(req,res) =>{
   }
 }`
 
-const city = `${req.query.name}%`
 const variables = {
-  search: city,
+  search: `${req.query.name}%`,
   limit:4
 }
-//
-console.log(variables)
 
 const data = await graphqlClient.request(query,variables)
-//console.log(data)
+console.log(data)
 res.send(data);
-//res.send(JSON.stringify(data, undefined, 2))
-//main().catch((error) => console.error(error))
-
-  // const returnData = await fetch(endpoint) 
-  // const output =  await returnData.json()
-  // res.send(output)
 })
  app.listen(port, () => {
  console.log(`app listening at http://localhost:${port}`)
