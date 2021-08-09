1d0c2aca4f242beb36d10a68eb6a22a1658f2043
Kiruthika202 <87745951+Kiruthika202@users.noreply.github.com>
2021-08-09 14:06:11 +0530

    sql

--- databases/studentCollection.sql
+++ databases/studentCollection.sql
@@ -1,45 +1,43 @@
      CREATE TABLE branch(
      id int PRIMARY KEY,
-    name varchar(20)
+     branch_name varchar(20)
      );
      
-     DROP TABLE branch;
-    INSERT INTO branch(id,name)
-    VALUES (1,'CSE');
-    INSERT INTO branch(id,name)
-    VALUES( 2,'IT');
+      INSERT INTO branch(id,branch_name)
+      VALUES (1,'CSE'), (2,'IT');
+       
+      CREATE TABLE student(
+      id int PRIMARY KEY,
+      student_name varchar(20)
+      ); 
 
-    CREATE TABLE student(
-    id int PRIMARY KEY,
-    name varchar(20)
-    ); 
     
-    INSERT INTO student(id,name) 
+    INSERT INTO student(id,student_name) 
     VALUES ( 1, 'Jay' ),( 2, 'Sanjay' ),( 3, 'Rajesh' );
     
-    CREATE TABLE branchStudent(
-    id int NOT NULL,
-    branch_id int,
-    student_id int,
-    PRIMARY KEY(id),
-    FOREIGN KEY (branch_id) REFERENCES branch(id),
-    FOREIGN KEY (branch_id) REFERENCES student(id)
-    
-    );
-    INSERT INTO branchStudent(id,branch_id,student_id)
-    VALUES (1, 1, 1 ),(2, 2, 2 ),( 3, 2, 1) ,( 4, 1,3);
-    
-    
-    SELECT * FROM branchStudent;
--   SELECT * FROM branch;
-    SELECT * FROM student;
-    -- inner join//for join table
---     SELECT Orders.OrderID, Customers.CustomerName, Shippers.ShipperName
--- FROM ((Orders
--- INNER JOIN Customers ON Orders.CustomerID = Customers.CustomerID)
--- INNER JOIN Shippers ON Orders.ShipperID = Shippers.ShipperID);
+      CREATE TABLE branch_Student(
+      id int ,
+      branch_Id int,
+      student_Id int,
+      PRIMARY KEY(id),
+      FOREIGN KEY (branch_Id) REFERENCES branch(Id),
+      FOREIGN KEY (branch_Id) REFERENCES student(Id)
+      );
 
-    SELECT branchStudent.id,branchStudent.branch_id,branch.name,branchStudent.student_id,student.name
-    FROM ((branchStudent
+    INSERT INTO branch_Student(id,branch_Id,student_Id)
+    VALUES (1, 1, 1 ),(2, 2, 2 ),( 3, 2, 1) ,( 4, 1,3),(5,null,null),(6,null,1),(7,1,null);
+    
+    
+      SELECT * FROM branch;
+     SELECT * FROM student;
+    
+      SELECT * FROM branch_Student;
+    
+    -- inner join//for join table
+
+    SELECT branch_Student.id,branch_Student.branch_id,branch.branch_name,branch_Student.student_id,student.student_name
+    FROM ((branch_Student
     INNER JOIN branch ON branch.id = branch_id)
-    INNER JOIN student ON student.id = student_id)   
+    INNER JOIN student ON student.id = student_id)  
+    
+    
