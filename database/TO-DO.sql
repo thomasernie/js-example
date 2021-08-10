3fd8d1ad67c7aa5f10436c5c12eef106780215f4
Dhanalaxmi-Shettiyar <dhanalaxmi.s@fr8.in>
2021-08-11 00:36:58 +0530

    Update TO-DO.sql

--- database/TO-DO.sql
+++ database/TO-DO.sql
@@ -1,75 +1,89 @@
 
-CREATE Table person(
-  id int NOT NULL,
-  name varchar,
-  PRIMARY KEY(id)
-)
+-- CREATE Table person(
+--   id int NOT NULL,
+--   name varchar NOT NULL,
+--   PRIMARY KEY(id)
+-- )
 
-CREATE Table todo(    
-  id int,           
-  person_id int,
-  description varchar,
-  priority_id int,
-  category_id int,
-  status_id int,
-  due_date timestamp,
-  PRIMARY KEY (person_id)
-  FOREIGN KEY (priority_id) REFERENCES priority(id),
-  FOREIGN KEY (category_id) REFERENCES category(id),
-  FOREIGN KEY (status_id) REFERENCES status(id)
-)
-CREATE Table category(
-  id int,
-  name varchar,
-  PRIMARY KEY(id)
-)
+-- CREATE Table todo(    
+--   id int NOT NULL,
+--   person_id int NOT NULL,
+--   priority_id int NOT NULL,
+--   category_id int NOT NULL,
+--   status_id int NOT NULL,
+--   description varchar ,
+--   due_date date NOT NULL,
+--   PRIMARY KEY (id),
+--   FOREIGN KEY (person_id) REFERENCES person(id),
+--   FOREIGN KEY (priority_id) REFERENCES priority(id),
+--   FOREIGN KEY (category_id) REFERENCES category(id),
+--   FOREIGN KEY (status_id) REFERENCES status(id)
+-- )
+
+-- CREATE Table category(
+--   id int NOT NULL,
+--   name varchar NOT NULL,
+--   PRIMARY KEY(id)
+-- )
  
-CREATE Table priority(
-  id int,
-  level varchar,
-  PRIMARY KEY(id)
-)
+-- CREATE Table priority(
+--   id int NOT NULL,
+--   level varchar NOT NULL,
+--   PRIMARY KEY(id)
+-- )
 
-CREATE Table status(
-  id int,
-  name varchar,
-  PRIMARY KEY(id)
-)
+-- CREATE Table status(
+--   id int NOT NULL,
+--   name varchar NOT NULL,
+--   PRIMARY KEY(id)
+-- )
 
-INSERT INTO person(id,name) 
-VALUES
-(1,'VASANTH')
-(2,'JAY') 
-(3,'SATISH') 
+-- INSERT INTO person(id,name) 
+-- VALUES
+--(1,'VASANTH')
+--(2,'JAY') 
+--(3,'SATISH') 
 
-INSERT INTO category(id,name) 
-VALUES
-(1,'Operations')
-(2,'IT')
+-- INSERT INTO category(id,name) 
+-- VALUES
+--(1,'Operations')
+--(2,'IT')
 -- (3,'IT')
 
-INSERT INTO priority(id,level) 
-VALUES
-(1,'High')
-(2,'Medium')
-(3,'Low')
+-- INSERT INTO priority(id,level) 
+-- VALUES
+--(1,'High')
+--(2,'Medium')
+--(3,'Low')
 
-INSERT INTO status(id,name) 
-VALUES
-(1,'Completed')
-(2,'In-progress')
-(3,'Not-Completed')
+-- INSERT INTO status(id,name) 
+-- VALUES
+-- (1,'Completed')
+-- (2,'In-progress')
+-- (3,'Not-Completed')
 
-INSERT INTO todo(id,person_id,description,due_date)
-VALUES
-(4,1,' Need to achieve 800 orders',30-09-2021)
-(5,2,'Train 5 interns','30-09-2021')
-(6,3,'Get trained in devops','30-09-2021')
+-- INSERT INTO todo(id,person_id,priority_id,category_id,status_id,description,due_date)
+-- VALUES
+--(1,1,1,1,1,' Need to achieve 800 orders','2021-09-30')
+--(2,2,2,2,2,'Train 5 interns','2021-09-30')
+--(3,3,3,3,3,'Get trained in devops','2021-09-30')
 
-SELECT person.id,person.name,todo.description,category.name,
-priority.level,status.name
-FROM todo
-INNER JOIN category ON category.id = todo.category_id
-INNER JOIN priority ON priority.id = todo.priority_id
-INNER JOIN status ON status.id = todo.status_id
+-- SELECT todo.description,category.name,
+-- priority.level,status.name
+-- FROM todo
+-- INNER JOIN category ON category.id = todo.category_id
+-- INNER JOIN priority ON priority.id = todo.priority_id
+-- INNER JOIN status ON status.id = todo.status_id
 
+-- SELECT person.id,person.name,todo.description,priority.id
+-- FROM todo
+-- INNER JOIN person ON person.id = todo.person_id
+-- --INNER JOIN category ON category.id = todo.category_id
+-- INNER JOIN priority ON priority.id = todo.priority_id
+-- -- INNER JOIN status ON status.id = todo.status_id
+-- ORDER BY person.id;
+
+SELECT person.id,person.name
+from person
+-- INNER JOIN person ON person.id = todo.person_id
+-- INNER JOIN category ON category.id = todo.category_id
