52bfd13d284754e6579910fb38b7554f4a45a19d
vijayat2 <87650249+vijayat2@users.noreply.github.com>
2021-08-10 18:44:06 +0530

    name changes

--- learningexercise/sqlfiles/dbdiagram.md
+++ learningexercise/sqlfiles/dbdiagram.md
@@ -22,33 +22,38 @@ Ref: "branch"."id" < "branch_student"."branch_id"
 
 TODO EXERCISE
 
-
-Table owners {
-  id int [pk,increment]
+Table task {
+  id int [pk]
+  user_id int
   category_id int
-  user_id int 
   status_id int
-  task varchar
+  description varchar
   due_date date
 }
+Table user{
+  id int [pk]
+  name varchar
+}
+
 
 Table category{
-  categoryid int [pk]
-  category_name varchar
+  id int [pk]
+  name varchar
 }
 
-Ref: "category"."categoryid" < "owners"."category_id"
 
-Table users{
-  userid int [pk]
-  username varchar
-}
 
-Ref: "users"."userid" < "owners"."user_id"
+
 
 Table status{
   id int [pk]
-  work_status inprogress
+  status inprogress
 }
 
-Ref: "status"."id" < "owners"."status_id"
+
+
+Ref: "user"."id" < "task"."user_id"
+
+Ref: "category"."id" < "task"."category_id"
+
+Ref: "status"."id" < "task"."status_id"
--- learningexercise/sqlfiles/todo.sql
+++ learningexercise/sqlfiles/todo.sql
@@ -1,54 +1,49 @@
+create table "user"(
+id int primary key,
+name varchar
+)
+insert into "user"(id,name) values(1,'vasanth');
+insert into "user"(id,name) values(2,'jay');
+insert into "user"(id,name) values(3,'sathish');
 create table status(
 id int primary key,
 status varchar
 );
 insert into status(id,status) values(1,'in-progress')
-
-
-create table users(
-id int primary key,
-user_name varchar
-);
-insert into users(id,user_name) values(1,'vasanth');
-insert into users(id,user_name) values(2,'jay');
-insert into users(id,user_name) values(3,'sathish');
-
-select * from users
-
 create table category(
 id int primary key,
-category_name varchar
+name varchar
 );
-insert into category(id,category_name) values(1,'operations');
-insert into category(id,category_name) values(2,'IT');
-
-create table owners(
+insert into category(id,name) values(1,'operations');
+insert into category(id,name) values(2,'IT');
+create table task(
  id int primary key,
- user_id int ,
- task_id varchar,
+ user_id int,
  category_id int,
  status_id int,
+ description varchar,
  dueDate date,
- foreign key (user_id) references users (id),
+ foreign key (user_id) references "user" (id),
  foreign key (category_id) references category (id),
  foreign key (status_id) references status (id)
 );
-insert into owners(id,user_id,task_id,category_id,status_id,dueDate) values(1,1,'need to achieve 800 orders',1,1,'2021-09-30');
-insert into owners(id,user_id,task_id,category_id,status_id,dueDate) values(2,2,'train 5 interns',2,1,'2021-09-30');
-insert into owners(id,user_id,task_id,category_id,status_id,dueDate) values(3,3,'get trained in devops',2,1,'2021-09-30');
+
+insert into task(id,user_id,category_id,status_id,description,dueDate) values(1,1,1,1,'need to achieve 800 orders','2021-09-30');
+insert into task(id,user_id,category_id,status_id,description,dueDate) values(2,2,2,1,'train 5 interns','2021-09-30');
+insert into task(id,user_id,category_id,status_id,description,dueDate) values(3,3,2,1,'get trained in devops','2021-09-30');
+
+SELECT "user".name,description,category.name as category,status.status as status,dueDate
+FROM task
+inner join "user" on task.user_id="user".id
+inner join category on task.category_id=category.id
+inner join status on task.status_id=status.id
 
 
-SELECT user_name,task_id,category_name,status,dueDate
-FROM owners
-inner join users on owners.user_id=users.id
-inner join category on owners.category_id=category.id
-inner join status on owners.status_id=status.id
 
-ALTER TABLE owners 
-RENAME COLUMN task_id to task; 
 
-SELECT user_name,task,category_name,status,dueDate
-FROM owners
-inner join users on owners.user_id=users.id
-inner join category on owners.category_id=category.id
-inner join status on owners.status_id=status.id
+
+
+
+
+
+
