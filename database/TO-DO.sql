e95b119fb804f70cc57ee0f4ae9351ce9718527c
Dhanalaxmi-Shettiyar <dhanalaxmi.s@fr8.in>
2021-08-11 17:44:27 +0530

    TODO

--- database/TO-DO.sql
+++ database/TO-DO.sql
@@ -1,18 +1,19 @@
+
 -- Create
 CREATE Table person(
-  id int NOT NULL,
-  name varchar NOT NULL,
+  id int,
+  name varchar,
   PRIMARY KEY(id)
 )
 
 CREATE Table todo(    
-  id int NOT NULL,
-  person_id int NOT NULL,
-  priority_id int NOT NULL,
-  category_id int NOT NULL,
-  status_id int NOT NULL,
+  id int,
+  person_id int,
+  priority_id int,
+  category_id int,
+  status_id int,
   description varchar ,
-  due_date date NOT NULL,
+  due_date date,
   PRIMARY KEY (id),
   FOREIGN KEY (person_id) REFERENCES person(id),
   FOREIGN KEY (priority_id) REFERENCES priority(id),
@@ -21,20 +22,20 @@ CREATE Table todo(
 )    
 
 CREATE Table category(
-  id int NOT NULL,
-  name varchar NOT NULL,
+  id int,
+  name varchar,
   PRIMARY KEY(id)
 )
  
 CREATE Table priority(
-  id int NOT NULL,
-  level varchar NOT NULL,
+  id int,
+  level varchar,
   PRIMARY KEY(id)
 )
 
 CREATE Table status(
-  id int NOT NULL,
-  name varchar NOT NULL,
+  id int,
+  name varchar,
   PRIMARY KEY(id)
 )
 
@@ -43,7 +44,10 @@ INSERT INTO person(id,name)
 VALUES
 (1,'VASANTH')
 (2,'JAY') 
-(3,'SATISH') 
+(3,'SATISH')
+(4,'Arjun')
+(5,'kiruthika')
+(6,'Dhanalaksmi')
 
 INSERT INTO category(id,name) 
 VALUES
@@ -62,18 +66,69 @@ VALUES
 (1,'Completed')
 (2,'In-progress')
 (3,'Not-Completed')
+(4,'on-hold')
 
-INSERT INTO todo(id,person_id,priority_id,category_id,status_id,description,due_date)
-VALUES
+ INSERT INTO todo(id,person_id,priority_id,category_id,status_id,description,due_date)
+ VALUES
 (1,1,1,1,1,' Need to achieve 800 orders','2021-09-30')
 (2,2,2,2,2,'Train 5 interns','2021-09-30')
 (3,3,3,2,3,'Get trained in devops','2021-09-30')
+(4,4,1,null,null,'Need to achieve all orders','2021-09-30')
  
+ALTER TABLE todo
+ADD customer_id int NULL
+
+ALTER TABLE todo
+ADD status_id int NULL
+
+ALTER TABLE category
+ADD id int NULL
+
+ALTER TABLE status
+ADD id int NULL
+
 --Select 
-SELECT person.id,person.name AS person_name,todo.description,category.id AS category_id,category.name AS 
-category_name,priority.level,status.name AS status_name,due_date
+-- INNER JOIN
+SELECT person.id,person.name AS person_name,todo.id AS todo_id,todo.description,todo.priority_id,
+priority.level,category.id AS category_id,category.name AS category_name,status.id AS status_id,status.name AS status_name
+FROM todo
+INNER JOIN person ON person.id = todo.person_id
+INNER JOIN priority ON priority.id = todo.priority_id
+INNER JOIN category ON category.id = todo.category_id
+INNER JOIN status ON status.id = todo.status_id
+ORDER BY person.id;
+
+-- FULL OUTER JOIN 
+SELECT person.id,person.name AS person_name,todo.id AS todo_id,todo.description,todo.priority_id,
+priority.level,category.id AS category_id,category.name AS category_name,status.id AS status_id,status.name AS status_name
 FROM todo
 FULL OUTER JOIN person ON person.id = todo.person_id
-INNER JOIN category ON category.id = todo.category_id
-INNER JOIN priority ON priority.id = todo.priority_id
-INNER JOIN status ON status.id = todo.status_id
+FULL OUTER JOIN priority ON priority.id = todo.priority_id
+FULL OUTER JOIN category ON category.id = todo.category_id
+FULL OUTER JOIN status ON status.id = todo.status_id
+ORDER BY person.id;
+
+-- RIGHT JOIN
+SELECT person.id,person.name AS person_name,todo.id AS todo_id,todo.description,todo.priority_id,
+priority.level,category.id AS category_id,category.name AS category_name,status.id AS status_id,status.name AS status_name
+FROM todo
+RIGHT JOIN person ON person.id = todo.person_id
+RIGHT JOIN priority ON priority.id = todo.priority_id
+RIGHT JOIN category ON category.id = todo.category_id
+RIGHT JOIN status ON status.id = todo.status_id
+ORDER BY person.id;
+
+--LEFT JOIN
+SELECT person.id,person.name AS person_name,todo.id AS todo_id,todo.description,todo.priority_id,
+priority.level,category.id AS category_id,category.name AS category_name,status.id AS status_id,status.name AS status_name
+FROM todo
+LEFT JOIN person ON person.id = todo.person_id
+LEFT JOIN priority ON priority.id = todo.priority_id
+LEFT JOIN category ON category.id = todo.category_id
+LEFT JOIN status ON status.id = todo.status_id
+ORDER BY person.id;
+
+
+
+
+
