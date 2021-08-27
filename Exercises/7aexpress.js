20d55d11764d79ced424fcea70d468a93d799091
DhanalaxmiShettiyar <dhanalaxmi.s@fr8.in>
2021-08-27 11:13:40 +0530

    updates

--- Exercises/7aexpress.js
+++ Exercises/7aexpress.js
@@ -11,44 +11,44 @@ const port = 3000
 //     res.send(`hello${queryParameter.name}`);
 // });
 
-// // To post Hello Jay
-// app.post('/hello',function (req,res){
-//    const postParameter = req.body
-//    console.log(postParameter)
-//    res.send(`{Output:hello${postParameter.name}}`)
-//  });
+// To post Hello Jay
+app.post('/hello',function (req,res){
+   const postParameter = req.body
+   console.log(postParameter)
+   res.send(`{Output:hello${postParameter.name}}`)
+ });
 
 // To post Student
-function studentInfo(studentDatas){
-const branchStudents = studentDatas.branchStudent;
-    const branches = studentDatas.branches;
-    const students = studentDatas.students;
+// function studentInfo(studentDatas){
+// const branchStudents = studentDatas.branchStudent;
+//     const branches = studentDatas.branches;
+//     const students = studentDatas.students;
     
-    const branchNameFinder = (branchId) => branches.find(branch => branch.id === branchId).name;
+//     const branchNameFinder = (branchId) => branches.find(branch => branch.id === branchId).name;
     
-    const studentNameFinder = (studentId) => students.find(student => student.id === studentId).name;
+//     const studentNameFinder = (studentId) => students.find(student => student.id === studentId).name;
     
-    const branchStudentMapper = (branchStudents)  => 
-    { 
-        return {
-            id:branchStudents.id,
-            branchId:branchStudents.branchId,
-            branchName: branchNameFinder(branchStudents.branchId),
-            studentId: branchStudents.studentId,
-            studentName: studentNameFinder(branchStudents.studentId)
-        }
-    };
+//     const branchStudentMapper = (branchStudents)  => 
+//     { 
+//         return {
+//             id:branchStudents.id,
+//             branchId:branchStudents.branchId,
+//             branchName: branchNameFinder(branchStudents.branchId),
+//             studentId: branchStudents.studentId,
+//             studentName: studentNameFinder(branchStudents.studentId)
+//         }
+//     };
     
-    const branchStudentMaps = branchStudents.map(branchStudentMapper);
-    return branchStudentMaps;
+//     const branchStudentMaps = branchStudents.map(branchStudentMapper);
+//     return branchStudentMaps;
     
-};
+// };
 
-app.post('/student', function(req,res){
-    const input = req.body
-    //console.log(studentInfo(input))
-    res.send(studentInfo(input))
-});
+// app.post('/student', function(req,res){
+//     const input = req.body
+//     //console.log(studentInfo(input))
+//     res.send(studentInfo(input))
+// });
 app.listen(port, () => {
     console.log(`app listening at http://localhost:${port}`)
 })
--- ExpressSelf/ex3.js
+++
@@ -1,6 +0,0 @@
-var http = require('http');
-http.createServer(function (req, res) {
-  res.writeHead(200, {'Content-Type': 'text/html'});
-  res.write("write html code to display you test")
-  res.end();
-}).listen(8080);
